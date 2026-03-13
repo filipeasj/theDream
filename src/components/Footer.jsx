e0401417a95f2bff8a3330c6d3cd976b7a9b3a11
filipeasj <filipealves.profissional@gmail.com>
2026-03-13 11:59:38 -0300

    perf: menos requisições e html atualizado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
+
 export default function Footer() {
   return (
     <footer className="relative py-4 text-center">
@@ -6,16 +8,16 @@ export default function Footer() {
       </p>
 
       <div className="flex justify-center space-x-4 mt-2">
-        <a href="https://www.linkedin.com/in/filipeasj/" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-linkedin"></i>
+        <a href="https://www.linkedin.com/in/filipeasj/" target="_blank" rel="noopener noreferrer" aria-label="Perfil de Filipe Alves no linkedin">
+          <FaLinkedin/>
         </a>
 
-        <a href="https://www.instagram.com/filipeasj/" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-instagram"></i>
+        <a href="https://www.instagram.com/filipeasj/" target="_blank" rel="noopener noreferrer" aria-label="Perfil de Filipe Alves no instagram">
+          <FaInstagram/>
         </a>
 
-        <a href="https://github.com/filipeasj" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-github"></i>
+        <a href="https://github.com/filipeasj" target="_blank" rel="noopener noreferrer" aria-label="Perfil de Filipe Alves no GitHub">
+          <FaGithub/>
         </a>
       </div>
     </footer>
